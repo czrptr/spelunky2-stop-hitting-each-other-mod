60fb89b3f2b72ed6debb5a3aec84fb97a168fab5
czrptr <cezar.c.petriuc@gmail.com>
2025-10-05 15:28:40 +0300

    Systems: Prevent firendly damage for pets and tamed mounts

--- source/main.lua
+++ source/main.lua
@@ -6,58 +6,113 @@ meta = {
   author = "Quasar",
 }
 
----@param uid integer
+-- ==============================================================================
+
+---@param entity Entity
 ---@return boolean
-local function is_player(uid)
-  for _, player in ipairs(get_local_players()) do
-    if player.uid == uid then
-      return true
-    end
-  end
-  return false
+local function is_player(entity)
+  ---@diagnostic disable-next-line undefined-field
+  return entity.get_short_name ~= nil
+end
+
+---@param entity Entity
+---@return boolean
+local function is_pet(entity)
+  ---@diagnostic disable-next-line undefined-field
+  return entity.petted_counter ~= nil
+end
+
+---@param entity Entity
+---@return boolean
+local function is_mount(entity)
+  ---@diagnostic disable-next-line undefined-field
+  return entity.tamed ~= nil
 end
 
 ---@param entity_type ENT_TYPE
 ---@return boolean
 local function is_exmplosion(entity_type)
-  return entity_type == ENT_TYPE.FX_EXPLOSION
+  return
+      entity_type == ENT_TYPE.FX_EXPLOSION
       or entity_type == ENT_TYPE.FX_POWEREDEXPLOSION
       or entity_type == ENT_TYPE.FX_MODERNEXPLOSION
 end
 
----@param victim Movable
+-- ==============================================================================
+
+---@param victim Player
 ---@param attacker Movable
----@param damage_amount integer
----@param damage_flags DAMAGE_TYPE
----@param velocity Vec2
----@param unknown_damage_phase integer
----@param stun_amount integer
----@param iframes integer
----@param unknown_is_final boolean
 ---@return boolean?
-local function on_pre_damage(
-    victim, attacker, damage_amount, damage_flags,
-    velocity, unknown_damage_phase, stun_amount,
-    iframes, unknown_is_final)
+local function on_player_pre_damage(victim, attacker)
   if attacker == nil then
     return nil -- allow damage from environment
   end
 
-  if is_player(victim.uid) then
-    if is_player(attacker.uid) then
+  if is_player(attacker) then
+    return false
+  end
+
+  if is_player(get_entity(attacker.last_owner_uid)) then
+    ---@diagnostic disable-next-line unknowd_field
+    if attacker.shot_from_trap then
+      return nil -- arrows shot from traps hurt
+    end
+    if is_exmplosion(attacker.type.id) then
+      return nil -- explosions from player caused explosions hurt
+    end
+    return false
+  end
+
+  return nil
+end
+
+---@param victim Pet
+---@param attacker Movable
+---@return boolean?
+local function on_pet_pre_damage(victim, attacker)
+  if attacker == nil then
+    return nil -- allow damage from environment
+  end
+
+  if is_player(attacker) then
+    return false
+  end
+
+  if is_player(get_entity(attacker.last_owner_uid)) then
+    ---@diagnostic disable-next-line unknowd_field
+    if attacker.shot_from_trap then
+      return nil -- arrows shot from traps hurt
+    end
+    if is_exmplosion(attacker.type.id) then
+      return nil -- explosions from player caused explosions hurt
+    end
+    return false
+  end
+
+  return nil
+end
+
+---@param victim Mount
+---@param attacker Movable
+---@return boolean?
+local function on_mount_pre_damage(victim, attacker)
+  if attacker == nil then
+    return nil -- allow damage from environment
+  end
+
+  if victim.tamed then
+    if is_player(attacker) then
       return false
     end
 
-    if is_player(attacker.last_owner_uid) then
+    if is_player(get_entity(attacker.last_owner_uid)) then
       ---@diagnostic disable-next-line unknowd_field
       if attacker.shot_from_trap then
         return nil -- arrows shot from traps hurt
       end
-
       if is_exmplosion(attacker.type.id) then
         return nil -- explosions from player caused explosions hurt
       end
-
       return false
     end
   end
@@ -73,16 +128,39 @@ local function on_post_pickup(self, entity)
   end
 
   ---@cast entity Arrow
-  entity.shot_from_trap = not is_player(self.uid)
+  entity.shot_from_trap = not is_player(self)
 end
 
-set_post_entity_spawn(function(entity, spawn_flags)
+---@param entity Entity
+local function on_spawn(entity)
   ---@diagnostic disable-next-line unknowd_field
   if entity.set_pre_damage == nil then
     return
   end
-
   ---@cast entity Movable
-  entity:set_pre_damage(on_pre_damage)
   entity:set_post_pick_up(on_post_pickup)
-end, SPAWN_TYPE.ANY, MASK.ANY)
+
+  if is_player(entity) then
+    entity:set_pre_damage(on_player_pre_damage)
+  elseif options.spare_pets and is_pet(entity) then
+    entity:set_pre_damage(on_pet_pre_damage)
+  elseif options.spare_mounts and is_mount(entity) then
+    entity:set_pre_damage(on_mount_pre_damage)
+  end
+end
+
+-- ==============================================================================
+
+register_option_bool(
+  "spare_pets",
+  "Players can't damage pets",
+  true
+)
+
+register_option_bool(
+  "spare_mounts",
+  "Players can't damage tamed mounts",
+  true
+)
+
+set_post_entity_spawn(on_spawn, SPAWN_TYPE.ANY, MASK.ANY)
