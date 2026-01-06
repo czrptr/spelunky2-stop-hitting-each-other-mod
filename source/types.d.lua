6fb800f83c08a85bb113e8ac68bb65e31afc9f02
czrptr <cezar.c.petriuc@gmail.com>
2026-01-06 20:32:42 +0200

    Meta: Add type annotations

---
+++ source/types.d.lua
@@ -0,0 +1,15 @@
+---@meta
+
+---@class UserData
+---@field holder_uid integer
+
+---@class Entity
+---@diagnostic disable-next-line: duplicate-doc-field
+---@field user_data UserData | any
+
+---@class Options
+---@field spare_pets boolean
+---@field spare_mounts boolean
+
+---@type Options | any
+options = nil
