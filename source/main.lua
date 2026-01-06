346129a2bbb4ade5c5317fb052d53380fc58baca
czrptr <cezar.c.petriuc@gmail.com>
2026-01-06 21:00:21 +0200

    Systems: Player ridden turkeys don't deal damage to players

--- source/main.lua
+++ source/main.lua
@@ -51,6 +51,12 @@ local function should_block_player_damage(attacker)
     return false
   end
 
+  if attacker.type.id == ENT_TYPE.ITEM_TURKEY_NECK then
+    local turkey = get_entity(attacker.last_owner_uid) --[[@as Mount]]
+    local rider = get_entity(turkey.rider_uid)
+    return not is_player(rider)
+  end
+
   -- Check if projectile/weapon is owned by a player
   local owner = get_entity(attacker.last_owner_uid)
   -- Not player-owned, allow damage
